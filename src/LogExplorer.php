a669baf3df6e0d2fc26e1ae03b7313e86739e6d6
Rogério Gomes <rogerio.wcast@gmail.com>
2021-06-20 18:54:16 -0300

    Validar o diretorio

--- src/LogExplorer.php
+++ src/LogExplorer.php
@@ -2,8 +2,6 @@
 
 namespace WCast\Log;
 
-use Illuminate\Support\Facades\Storage;
-
 class LogExplorer
 {
 
@@ -14,41 +12,48 @@ class LogExplorer
 
     public function getFolders()
     {
-        $diretorio = storage_path().'/logs';
-        $scandir =  scandir($diretorio);
-        $scandir = array_filter($scandir, function($pasta){
+        $diretorio = storage_path() . '/logs';
+        $scandir = scandir($diretorio);
+        $scandir = array_filter($scandir, function ($pasta) {
             return !preg_match('/^\./', $pasta);
         });
         $pastas = [];
-        foreach($scandir as $pasta){
-            if(is_dir($diretorio . DIRECTORY_SEPARATOR . $pasta)){
+        foreach ($scandir as $pasta) {
+            if (is_dir($diretorio . DIRECTORY_SEPARATOR . $pasta)) {
                 $pastas[] = [
-                   'nome' => $pasta,
-                   'diretorio' => encrypt($diretorio . DIRECTORY_SEPARATOR . $pasta)
+                    'nome' => $pasta,
+                    'diretorio' => encrypt($diretorio . DIRECTORY_SEPARATOR . $pasta)
                 ];
             }
         }
         return $pastas;
     }
 
-    public function getFiles($data = null)
+    public function getFiles($diretorio = null)
     {
 
-        $scandir =  scandir(decrypt($diretorio));
-        $scandir = array_filter($scandir, function($arquivo){
-            return !preg_match('/^\./', $arquivo);
-        });
-        $arquivos = [];
-        foreach($scandir as $arquivo){
-            if(is_file(decrypt($diretorio).DIRECTORY_SEPARATOR.$arquivo)){
-                $arquivos[] = [
-                    'nome' => $arquivo,
-                    'arquivo' => $diretorio,
-                    'tamanho' => $this->converterBytes(filesize(decrypt($diretorio).DIRECTORY_SEPARATOR.$arquivo),2),
-                    'criado' => date('d/m/Y H:i:s',fileatime(decrypt($diretorio).DIRECTORY_SEPARATOR.$arquivo))
-                ];
+        if ($diretorio) {
+            $scandir = scandir(decrypt($diretorio));
+            $scandir = array_filter($scandir, function ($arquivo) {
+                return !preg_match('/^\./', $arquivo);
+            });
+            $arquivos = [];
+            foreach ($scandir as $arquivo) {
+                if (is_file(decrypt($diretorio) . DIRECTORY_SEPARATOR . $arquivo)) {
+                    $arquivos[] = [
+                        'nome' => $arquivo,
+                        'arquivo' => $diretorio,
+                        'tamanho' => $this->converterBytes(filesize(decrypt($diretorio) . DIRECTORY_SEPARATOR . $arquivo), 2),
+                        'criado' => date('d/m/Y H:i:s', fileatime(decrypt($diretorio) . DIRECTORY_SEPARATOR . $arquivo))
+                    ];
+                }
             }
+
+        } else {
+
+            $arquivos = [];
         }
+
         return $arquivos;
     }
 
@@ -56,6 +61,6 @@ class LogExplorer
     {
         $base = log($size, 1024);
         $suffixes = array('B', 'kB', 'MB', 'GB', 'TB');
-        return round(pow(1024, $base - floor($base)), $precision) .' '. $suffixes[floor($base)];
+        return round(pow(1024, $base - floor($base)), $precision) . ' ' . $suffixes[floor($base)];
     }
 }
